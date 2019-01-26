d035efe542434316aef070d64db181655bc13d10
CloversDev <cloversdevs@gmail.com>
2019-01-26 13:41:23 -0300

    ! Added human character controller WIP

---
+++ Assets/HumanCharacterController.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public delegate void ArrivalEvent();
+public class HumanCharacterController : MonoBehaviour 
+{
+	public Transform testTarget;
+	public float MinDistance = 0.1f;
+	private NavMeshAgent agent;
+	void Awake () 
+	{
+		
+		NavMeshPath path = new NavMeshPath();
+		agent = GetComponent<NavMeshAgent>();	
+		
+		if(agent.CalculatePath(testTarget.position, path))
+		{
+			Debug.Log("Path found");
+			agent.SetPath(path);
+		}
+		else
+		{
+			Debug.Log("Path not found");
+		}
+	}
+	ArrivalEvent OnArrive;
+	public void SetDestination(Vector3 TargetPosition, ArrivalEvent cb = null)
+	{
+
+	}
+	void Update()
+	{
+		if(OnArrive!=null)
+		{
+			//if()
+		}
+	}
+}
