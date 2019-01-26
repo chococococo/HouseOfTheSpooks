ac0a72d7f452fd1085213381be8e4c5291054ab6
Nachoco <ignaciocozza@gmail.com>
2019-01-26 17:28:35 -0300

    Basic ghost + possessing

---
+++ Assets/Scripts/GhostMovement.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GhostMovement : MonoBehaviour {
+    public float speed = 2f;
+
+
+    DashController dash;
+    GhostController ghost;
+    Rigidbody rb;
+    // Use this for initialization
+    void Start() {
+        dash = GetComponent<DashController>();
+        ghost = GetComponent<GhostController>();
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update() {
+        Vector3 dir = Vector3.zero;
+        if (!dash.IsDashing()) {
+            dir = ghost.GetNextDir();
+            if (dir != Vector3.zero) {
+                rb.MovePosition(transform.position + dir * Time.deltaTime * speed);
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/PossessableProp.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PossessableProp : MonoBehaviour {
+
+    public float time = 1f;
+    public GameObject brokenProp;
+    float tmr;
+    GhostController ghost;
+
+    private void Start() {
+        tmr = 0f;
+    }
+
+    private void OnTriggerEnter(Collider other) {
+        if (other.CompareTag("Player")) {
+            Debug.Log("player entered");
+            ghost = other.transform.parent.GetComponent<GhostController>();
+            //PlayAnimation
+            ghost.StartPossess();
+        }
+    }
+
+    private void Update() {
+        if (ghost != null) {
+            tmr += Time.deltaTime;
+            if (tmr >= time) {
+                ghost.FinishPossess();
+                Instantiate(brokenProp, this.transform.position, this.transform.rotation);
+                Destroy(this.gameObject);
+            }
+        }
+    }
+
+}
